15cf81aadf7e11f9503fcd546d39f78ee6392771
Pratik-Honmane <pratikmhonmane@gmail.com>
2026-03-13 19:19:23 +0530

    ASCII value code is added

---
+++ AsciiValue.py
@@ -0,0 +1,14 @@
+# ASCII = AMERICAN STANDARD CODE FOR INFORMATION INTERCHANGE
+
+# ASCII value start from 
+
+# Important ASCII Ranges
+# Characters	ASCII Range
+# Control Characters =	0 – 31
+# Digits (0-9) =	48 – 57
+# Uppercase Letters (A-Z) =	65 – 90
+# Lowercase Letters (a-z) =	97 – 122
+
+print(ord('A')) # 65
+
+print(ord('a')) # 97

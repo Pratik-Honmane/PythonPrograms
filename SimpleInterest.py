559d08d02fa3efb32ec2b67ff339641d10a38c5c
Pratik-Honmane <pratikmhonmane@gmail.com>
2026-03-13 09:44:27 +0530

    Simple Interest Program is added

---
+++ SimpleInterest.py
@@ -0,0 +1,11 @@
+print("Enter your principle :")
+principle = int(input())
+
+print("Enter your rate : ")
+rate = float(input())
+
+print("Enter time :")
+time = int(input())
+
+simpleInterest = ((principle * rate * time)/100)
+print("The simple interest is : " , simpleInterest)

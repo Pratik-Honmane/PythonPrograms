6d4bbb8a758d1a29ce745d9cf6955c0fe4915ad1
Pratik-Honmane <pratikmhonmane@gmail.com>
2026-03-13 19:30:30 +0530

    ASCII value file is updated

--- AsciiValue.py
+++ AsciiValue.py
@@ -1,6 +1,6 @@
 # ASCII = AMERICAN STANDARD CODE FOR INFORMATION INTERCHANGE
 
-# ASCII value start from 
+# ASCII value start from 0 to 127
 
 # Important ASCII Ranges
 # Characters	ASCII Range
@@ -9,6 +9,12 @@
 # Uppercase Letters (A-Z) =	65 – 90
 # Lowercase Letters (a-z) =	97 – 122
 
+
+# check the character value use ord() function
 print(ord('A')) # 65
 
 print(ord('a')) # 97
+
+# convert ascii to character
+
+print(chr(97))
